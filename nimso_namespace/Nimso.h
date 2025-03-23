e5bc67b21b4c0aeb90dcd0849c6a35953a8d3f6f
nimsolated <armindoosti03@gmail.com>
2025-03-22 18:48:54 -0700

    Critical fix to emplace and push functions in the DoublyLinkedList class. Added find, findPos, findInsert, findDelete, and findReplace functions to the DoublyLinkedList class.

--- nimso_namespace/Nimso.h
+++ nimso_namespace/Nimso.h
@@ -2,6 +2,8 @@
 #include <iostream>
 
 namespace Nimso {
+    enum s_InsertionRules { INSERT_BEFORE = 0, INSERT_AFTER };
+
     template <typename Variant>
     struct Node {
         Node<Variant>* m_next = nullptr;
@@ -141,17 +143,15 @@ namespace Nimso {
         }
     };
     
-    // TODO: a delete function to delete a node that has matching data with the target data
-    // TODO: a find function to check if the target data exists in the list
     template <typename T>
-    class LinkedList {
+    class DoublyLinkedList {
     private:
         size_t m_size;
         DoublyNode<T>* m_head;
         DoublyNode<T>* m_tail;
     public:
-        LinkedList() : m_size(0), m_head(nullptr), m_tail(nullptr) {};
-        ~LinkedList() {
+        DoublyLinkedList() : m_size(0), m_head(nullptr), m_tail(nullptr) {};
+        ~DoublyLinkedList() {
             if (m_size == 0) {
                 return;
             }
@@ -186,6 +186,7 @@ namespace Nimso {
                 m_tail = newNode;
             }
             else {
+                m_head->m_prev = newNode;
                 newNode->m_next = m_head;
                 m_head = newNode;
             }
@@ -216,6 +217,7 @@ namespace Nimso {
                 m_tail = newNode;
             }
             else {
+                m_head->m_prev = newNode;
                 newNode->m_next = m_head;
                 m_head = newNode;
             }
@@ -279,7 +281,195 @@ namespace Nimso {
             m_size--;
             return r;
         }
-        void swap(LinkedList<T>& other) {
+        bool find(const T& targetData) {
+            if (m_size == 0) {
+                return false;
+            }
+
+            if (m_head->m_data == targetData) {
+                return true;
+            }
+            else if (m_tail->m_data == targetData) {
+                return true;
+            }
+
+            DoublyNode<T>* curr = m_head;
+            while (curr->m_next && curr->m_next != m_tail) {
+                curr = curr->m_next;
+                if (curr->m_data == targetData) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        long long int findPos(const T& targetData) {
+            if (m_size == 0) {
+                return -1;
+            }
+
+            if (m_head->m_data == targetData) {
+                return 0;
+            }
+            else if (m_tail->m_data == targetData) {
+                return m_size - 1;
+            }
+
+            DoublyNode<T>* curr = m_head;
+            size_t pos = 0;
+            while (curr->m_next && curr->m_next != m_tail) {
+                curr = curr->m_next;
+                pos++;
+                if (curr->m_data == targetData) {
+                    return pos;
+                }
+            }
+
+            return -1;
+        }
+        bool findInsert(const T& targetData, const T& dataToInsert, s_InsertionRules rule = INSERT_AFTER) {
+            if (m_size == 0) {
+                return false;
+            }
+
+            DoublyNode<T>* newNode = new DoublyNode<T>(dataToInsert);
+
+            if (m_head->m_data == targetData) {
+                if (rule == INSERT_AFTER) {
+                    newNode->m_next = m_head->m_next;
+                    m_head->m_next->m_prev = newNode;
+                    m_head->m_next = newNode;
+                    newNode->m_prev = m_head;
+                }
+                else if (rule == INSERT_BEFORE) {
+                    newNode->m_next = m_head;
+                    m_head->m_prev = newNode;
+                    m_head = newNode;
+                }
+                m_size++;
+                return true;
+            }
+            else if (m_tail->m_data == targetData) {
+                if (rule == INSERT_AFTER) {
+                    newNode->m_prev = m_tail;
+                    m_tail->m_next = newNode;
+                    m_tail = newNode;
+                }
+                else if (rule == INSERT_BEFORE) {
+                    newNode->m_next = m_tail;
+                    newNode->m_prev = m_tail->m_prev;
+                    m_tail->m_prev->m_next = newNode;
+                    m_tail->m_prev = newNode;
+                }
+                m_size++;
+                return true;
+            }
+
+            DoublyNode<T>* curr = m_head;
+            while (curr->m_next && curr->m_next != m_tail) {
+                curr = curr->m_next;
+                if (curr->m_data == targetData) {
+                    if (rule == INSERT_AFTER) {
+                        newNode->m_prev = curr;
+                        newNode->m_next = curr->m_next;
+                        curr->m_next->m_prev = newNode;
+                        curr->m_next = newNode;
+                    }
+                    else if (rule == INSERT_BEFORE) {
+                        newNode->m_next = curr;
+                        newNode->m_prev = curr->m_prev;
+                        curr->m_prev->m_next = newNode;
+                        curr->m_prev = newNode;
+                    }
+                    m_size++;
+                    return true;
+                }
+            }
+
+            if (!newNode->m_prev && !newNode->m_next) {
+                delete newNode;
+            }
+
+            return false;
+        }
+        bool findDelete(const T& targetData) {
+            if (m_size == 0) {
+                return false;
+            }
+
+            if (m_head->m_data == targetData) {
+                if (m_head == m_tail) {
+                    delete m_head;
+                }
+                else {
+                    DoublyNode<T>* tempnode = m_head;
+                    m_head->m_next->m_prev = nullptr;
+                    m_head = m_head->m_next;
+                    tempnode->m_next = nullptr;
+                    delete tempnode;
+                }
+                m_size--;
+                return true;
+            }
+            else if (m_tail->m_data == targetData) {
+                if (m_tail == m_head) {
+                    delete m_tail;
+                }
+                else {
+                    DoublyNode<T>* tempnode = m_tail;
+                    m_tail->m_prev->m_next = nullptr;
+                    m_tail = m_tail->m_prev;
+                    tempnode->m_prev = nullptr;
+                    delete tempnode;
+                }
+                m_size--;
+                return true;
+            }
+
+            DoublyNode<T>* curr = m_head;
+            while (curr->m_next && curr->m_next != m_tail) {
+                curr = curr->m_next;
+                if (curr->m_data == targetData) {
+                    curr->m_prev->m_next;
+                    curr->m_next;
+                    curr->m_prev->m_next = curr->m_next;
+                    curr->m_next->m_prev = curr->m_prev;
+                    curr->m_next = nullptr;
+                    curr->m_prev = nullptr;
+                    delete curr;
+                    m_size--;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        bool findReplace(const T& targetData, const T& replacementData) {
+            if (m_size == 0) {
+                return false;
+            }
+
+            if (m_head->m_data == targetData) {
+                m_head->m_data = replacementData;
+                return true;
+            }
+            else if (m_tail->m_data == targetData) {
+                m_tail->m_data = replacementData;
+                return true;
+            }
+
+            DoublyNode<T>* curr = m_head;
+            while (curr->m_next && curr->m_next != m_tail) {
+                curr = curr->m_next;
+                if (curr->m_data == targetData) {
+                    curr->m_data = replacementData;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        void swap(DoublyLinkedList<T>& other) {
             if (this != &other && (this->m_size > 0 || other.m_size > 0)) {
                 DoublyNode<T>* tempnode = this->m_head;
                 this->m_head = other.m_head;
@@ -292,7 +482,7 @@ namespace Nimso {
                 other.m_size = tempsizet;
             }
         }
-        friend std::ostream& operator<<(std::ostream& os, LinkedList<T>& LL) {
+        friend std::ostream& operator<<(std::ostream& os, DoublyLinkedList<T>& LL) {
             os << '[';
             if (LL.m_head) {
                 DoublyNode<T>* curr = LL.m_head;
--- nimso_namespace/main.cpp
+++ nimso_namespace/main.cpp
@@ -2,15 +2,36 @@
 
 int main()
 {
-	Nimso::LinkedList<int> myList;
+	Nimso::DoublyLinkedList<int> myList;
 	myList.emplace(3); myList.emplace(10); // [10, 3]
 	myList.emplaceBack(15); myList.emplaceBack(27); // [10, 3, 15, 27]
 	myList.pop(); // [3, 15, 27]
 	myList.popBack(); // [3, 15]
 	int num = 8;
-	myList.push(num);
+	myList.push(num); // [8, 3, 15]
 
-	std::cout << myList << std::endl;
+	std::cout << myList << std::endl << std::endl;
+
+	std::cout << "found 3: " << myList.find(3) << std::endl
+			  << "found 27: " << myList.find(27) << std::endl;
+
+	std::cout << "15\'s position: " << myList.findPos(15) << std::endl
+			  << "8\'s position: " << myList.findPos(8) << std::endl
+			  << "27\'s position: " << myList.findPos(27) << std::endl;
+
+	std::cout << "deleted 3: " << myList.findDelete(3) << std::endl; // [8, 15]
+	std::cout << "deleted 99: " << myList.findDelete(99) << std::endl;
+	std::cout << "inserted 19 after 8: "
+			  << myList.findInsert(8, 19, Nimso::INSERT_AFTER) << std::endl; // [8, 19, 15]
+	std::cout << "inserted 42 before 15: "
+			  << myList.findInsert(15, 42, Nimso::INSERT_BEFORE) << std::endl; // [8, 19, 42, 15]
+	std::cout << "inserted 1 before 42: "
+			  << myList.findInsert(42, 1, Nimso::INSERT_BEFORE) << std::endl; // [8, 19, 1, 42, 15]
+
+	std::cout << "replaced 42 with 6: " << myList.findReplace(42, 6) << std::endl; // [8, 19, 1, 6, 15]
+
+	std::cout << std::endl << myList << std::endl;
+
 	std::cout << "H: " << myList.head() << std::endl
 			  << "T: " << myList.tail() << std::endl;
 }
